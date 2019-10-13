c448464462c0d07ebf5d591559b0a33345daa76e
oksanarutska <42785757+oksanarutska@users.noreply.github.com>
2019-10-13 18:59:02 +0300

    added build

---
+++ build/precache-manifest.4ec9ce3b124508a8e7d813fa45cc5d69.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07052fcf55d53457f72cadebd2b34840",
+    "url": "/uber-eats-react/build/index.html"
+  },
+  {
+    "revision": "f83cacd46f85c219e1eb",
+    "url": "/uber-eats-react/build/static/css/main.3caf8e92.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/uber-eats-react/build/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "f83cacd46f85c219e1eb",
+    "url": "/uber-eats-react/build/static/js/main.dfc4b42f.chunk.js"
+  },
+  {
+    "revision": "4aa0f08206259ca90be2",
+    "url": "/uber-eats-react/build/static/js/runtime~main.23339ce0.js"
+  },
+  {
+    "revision": "4945518c383891caee4f2539c671336e",
+    "url": "/uber-eats-react/build/static/media/druzi.4945518c.png"
+  },
+  {
+    "revision": "aedf4a97870a6d184c4c7c05dbfd0e7e",
+    "url": "/uber-eats-react/build/static/media/mak.aedf4a97.png"
+  },
+  {
+    "revision": "85fb9ef5e03755ed42bf925642743580",
+    "url": "/uber-eats-react/build/static/media/milk_bar.85fb9ef5.png"
+  },
+  {
+    "revision": "05f1bcdcfb84a125330be4ccfcd5ce5d",
+    "url": "/uber-eats-react/build/static/media/mimosa.05f1bcdc.png"
+  },
+  {
+    "revision": "76a912b8bdb0253c88da301921e3a402",
+    "url": "/uber-eats-react/build/static/media/musafir.76a912b8.png"
+  },
+  {
+    "revision": "b908a5607acbcf088517659cd271c3c6",
+    "url": "/uber-eats-react/build/static/media/to_be.b908a560.png"
+  },
+  {
+    "revision": "b95f20a7479984b1b4e8a4269d4ff427",
+    "url": "/uber-eats-react/build/static/media/wokwei.b95f20a7.png"
+  },
+  {
+    "revision": "66e50e84c3ecf5b6651976b9f9f8363e",
+    "url": "/uber-eats-react/build/static/media/wokwei_2.66e50e84.png"
+  },
+  {
+    "revision": "32da4c74976a786190e654e7b3629a22",
+    "url": "/uber-eats-react/build/static/media/yizha.32da4c74.png"
+  }
+]);
